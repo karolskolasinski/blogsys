5c49aefd28d332f243a82ea9403285ac023cb472
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-06-17 09:43:45 +0200

    feat: firebase login

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,6 @@ import localFont from "next/font/local";
 import { Open_Sans, Work_Sans } from "next/font/google";
 import "leaflet/dist/leaflet.css";
 import Footer from "@/components/Footer";
-import { NextResponse } from "next/server";
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
-import { db } from "@/lib/firebase";
 
 const calSans = localFont({
   src: "../public/fonts/CalSans-Regular.woff2",
@@ -45,28 +42,6 @@ export const metadata: Metadata = {
 };
 
 export default async function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
-  const adminLogin = process.env.INITIAL_ADMIN_LOGIN;
-  const adminPassword = process.env.INITIAL_ADMIN_PASSWORD;
-
-  if (!adminLogin || !adminPassword) {
-    return NextResponse.json({ error: "Missing credentials" }, { status: 500 });
-  }
-
-  const usersRef = collection(db, "users");
-  const q = query(usersRef, where("email", "==", adminLogin));
-  const snapshot = await getDocs(q);
-
-  if (snapshot.empty) {
-    const a = await addDoc(usersRef, {
-      login: adminLogin,
-      password: adminPassword,
-      role: "admin",
-      createdAt: Date.now(),
-    });
-
-    console.log("Admin user created: ", a);
-  }
-
   return (
     <html lang="pl" className={`${calSans.variable} ${workSans.variable} ${openSans.className}`}>
       <body className="antialiased bg-slate-50 min-h-[100vh] flex flex-col">
