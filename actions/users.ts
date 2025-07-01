0e918f3215483358fd8e6460dc61cf7c2745b780
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-07-01 13:32:13 +0200

    feat: init

--- actions/users.ts
+++ actions/users.ts
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db";
 import { User } from "@/types/common";
+import bcrypt from "bcryptjs";
 
 export async function getUserByEmail(email: string) {
   try {
@@ -17,8 +18,53 @@ export async function getUserByEmail(email: string) {
       ...userData as User,
       id: userDoc.id,
     };
-  } catch (error) {
-    console.error("Error getting user: ", error);
+  } catch (err) {
+    console.error("Error getting user: ", err);
     return null;
+  }
+}
+
+export async function init(formData: FormData) {
+  const email = formData.get("email");
+  const password = formData.get("password");
+  const key = formData.get("key");
+  const secretKey = process.env.INIT_ADMIN_SECRET_KEY;
+
+  if (!secretKey) {
+    throw new Error("Missing server key configuration");
+  }
+
+  if (key !== secretKey) {
+    throw new Error("Unauthorized");
+  }
+
+  if (!email || !password) {
+    throw new Error("Email and password required");
+  }
+
+  if (typeof email !== "string" || typeof password !== "string") {
+    throw new Error("Invalid email or password");
+  }
+
+  try {
+    const usersRef = db.collection("users");
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user: User = {
+      email,
+      password: hashedPassword,
+      name: "Admin",
+      role: "admin",
+    };
+    const querySnapshot = await usersRef.add(user);
+
+    return { uid: querySnapshot.id };
+  } catch (err: any) {
+    console.error("Error creating user: ", err);
+
+    if (err.code === "auth/email-already-exists") {
+      throw new Error("Użytkownik z takiem adresem email już istnieje");
+    }
+
+    throw new Error("Wystąpił błąd");
   }
 }
--- app/init/page.tsx
+++ app/init/page.tsx
@@ -1,50 +1,16 @@
-"use client";
-
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { init } from "@/actions/users";
 
 export default function InitAdminPage() {
-  const router = useRouter();
-
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [key, setKey] = useState("");
-  const [status, setStatus] = useState<"idle" | "success" | "error">("idle");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-    setStatus("idle");
-    setMessage("");
-
-    try {
-      const res = await fetch("/api/init", {  // todo: server action instead of fetch
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({ email, password, key }),
-      });
-
-      const data = await res.json();
-
-      if (res.ok) {
-        setStatus("success");
-      } else {
-        setStatus("error");
-        setMessage(data.error || "Wystąpił nieznany błąd.");
-      }
-    } catch (err) {
-      setStatus("error");
-      setMessage(err instanceof Error ? err.message : "Wystąpił nieznany błąd.");
-    }
-  };
-
   return (
     <main className="flex-1">
       <section className="mx-auto max-w-7xl px-4">
         <h1 className="text-3xl font-bold mb-4 mt-16 w-full">Tworzenie konta startowego</h1>
 
         <form
-          onSubmit={handleSubmit}
+          action={async (formData) => {
+            "use server";
+            await init(formData);
+          }}
           className="flex flex-col w-full lg:w-1/2 gap-4"
         >
           <div>
@@ -52,8 +18,7 @@ export default function InitAdminPage() {
             <input
               className="w-full h-10 p-2 mt-2 bg-white border border-gray-300 rounded-md"
               type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              name="email"
               required
             />
           </div>
@@ -63,8 +28,7 @@ export default function InitAdminPage() {
             <input
               className="w-full h-10 p-2 mt-2 bg-white border border-gray-300 rounded-md"
               type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              name="password"
               required
             />
           </div>
@@ -74,8 +38,7 @@ export default function InitAdminPage() {
             <input
               className="w-full h-10 p-2 mt-2 bg-white border border-gray-300 rounded-md"
               type="password"
-              value={key}
-              onChange={(e) => setKey(e.target.value)}
+              name="key"
               required
             />
           </div>
@@ -84,19 +47,19 @@ export default function InitAdminPage() {
             Stwórz konto administratora
           </button>
 
-          {status === "success" && (
-            <div className="mt-4 text-green-600">
-              Konto administratora zostało utworzone.
-              <button
-                className="block mt-2 text-blue-600 underline"
-                onClick={() => router.push("/login")}
-              >
-                Przejdź do logowania
-              </button>
-            </div>
-          )}
+          {/*{status === "success" && (*/}
+          {/*  <div className="mt-4 text-green-600">*/}
+          {/*    Konto administratora zostało utworzone.*/}
+          {/*    <button*/}
+          {/*      className="block mt-2 text-blue-600 underline"*/}
+          {/*      onClick={() => router.push("/login")}*/}
+          {/*    >*/}
+          {/*      Przejdź do logowania*/}
+          {/*    </button>*/}
+          {/*  </div>*/}
+          {/*)}*/}
 
-          {status === "error" && <div className="mt-4 text-red-600">Błąd: {message}</div>}
+          {/*{status === "error" && <div className="mt-4 text-red-600">Błąd: {message}</div>}*/}
         </form>
       </section>
     </main>
