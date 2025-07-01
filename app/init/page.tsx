a3f0a8d587653f4698c5a1d3e25db41c50ef6357
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-07-01 18:47:24 +0200

    feat: init page

--- app/init/page.tsx
+++ app/init/page.tsx
@@ -4,21 +4,24 @@ export default async function InitAdminPage() {
   return (
     <main className="flex-1">
       <section className="mx-auto max-w-7xl px-4">
-        <h1 className="text-3xl font-bold mb-4 mt-16 w-full">Tworzenie konta startowego</h1>
-
         <form
           action={async (formData) => {
             "use server";
             await init(formData);
           }}
-          className="flex flex-col w-full lg:w-1/2 gap-4"
+          className="flex-1 flex flex-col w-full lg:w-5/12 gap-4 mx-auto"
         >
+          <h1 className="text-3xl font-bold mb-4 mt-16 text-center">
+            Tworzenie konta startowego
+          </h1>
+
           <div>
             <label className="block text-sm text-gray-600">Email</label>
             <input
-              className="w-full h-10 p-2 mt-2 bg-white border border-gray-300 rounded-md"
+              className="w-full h-10 p-2 mt-2 border border-gray-300 rounded-md"
               type="email"
               name="email"
+              maxLength={255}
               required
             />
           </div>
@@ -26,9 +29,10 @@ export default async function InitAdminPage() {
           <div>
             <label className="block text-sm text-gray-600">Hasło</label>
             <input
-              className="w-full h-10 p-2 mt-2 bg-white border border-gray-300 rounded-md"
+              className="w-full h-10 p-2 mt-2 border border-gray-300 rounded-md"
               type="password"
               name="password"
+              maxLength={255}
               required
             />
           </div>
@@ -36,16 +40,20 @@ export default async function InitAdminPage() {
           <div>
             <label className="block text-sm text-gray-600">Klucz autoryzacyjny</label>
             <input
-              className="w-full h-10 p-2 mt-2 bg-white border border-gray-300 rounded-md"
+              className="w-full h-10 p-2 mt-2 border border-gray-300 rounded-md"
               type="password"
               name="key"
+              maxLength={255}
+              autoComplete="off"
               required
             />
           </div>
 
-          <button type="submit" className="button mt-4">
-            Stwórz konto administratora
-          </button>
+          <div className="flex justify-center">
+            <button type="submit" className="button mt-4">
+              Stwórz konto administratora
+            </button>
+          </div>
         </form>
       </section>
     </main>
