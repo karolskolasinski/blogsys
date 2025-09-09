548cc8be7246e6fcc38e118a00dd2d7016a4972c
Karol Skolasiński <karol.skolasinski@7willows.com>
2025-09-09 15:16:19 +0200

    feat: style active menu item

--- components/blogsys/MenuItem.tsx
+++ components/blogsys/MenuItem.tsx
@@ -14,7 +14,8 @@ type MenuItemProps = {
 };
 
 export default function MenuItem(props: MenuItemProps) {
-  const { href, label, icon, role } = props;
+  const { href, label, icon, role, active } = props;
+  const activeClass = active === href ? "bg-gray-100" : "";
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   if (role !== "admin" && href === "/users") {
@@ -25,7 +26,7 @@ export default function MenuItem(props: MenuItemProps) {
     <button
       onClick={() => startTransition(() => router.push(props.href))}
       disabled={isPending}
-      className="flex items-center gap-2 p-4 duration-100 ease-in-out font-bold hover:bg-gray-100 rounded-xl cursor-pointer"
+      className={`flex items-center gap-2 p-4 duration-100 ease-in-out font-bold hover:bg-gray-100 rounded-xl cursor-pointer ${activeClass}`}
     >
       {isPending ? <SpinnerIcon className="w-6 h-6 fill-gray-500 animate-spin" /> : icon}
       <span className="text-left whitespace-nowrap">
